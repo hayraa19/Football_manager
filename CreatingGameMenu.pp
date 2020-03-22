152b3d45697ba82ede157ecb68708fba47b0b540
hayraa19 <62447305+hayraa19@users.noreply.github.com>
2020-03-22 17:33:23 +0000

    Update and rename CreatingGameMenu.sln to CreatingGameMenu.pp

---
+++ CreatingGameMenu.pp
@@ -0,0 +1,53 @@
+#include"Header.h"
+
+
+using namespace std;
+//function definitions
+
+void displayMenu() {
+
+	cout << " ___________________" << endl;
+	cout << "|1. Start New Game /" << endl;
+	cout << "|_________________/    " << endl;
+	cout << " ___________________" << endl;
+ 	cout << "|2. Resume Game    /" << endl;
+	cout << "|_________________/    " << endl;
+	cout << " ___________________" << endl;
+	cout << "|3. Delete Game    /" << endl;
+	cout << "|_________________/    " << endl;
+	cout << " ___________________" << endl;
+	cout << "|4. Quit Game      /" << endl;
+	cout << "|_________________/    " << endl;
+	cout << " ___________________" << endl;
+	cout << "|5. Settings       /" << endl;
+	cout << "|_________________/    " << endl;
+}
+
+void enterChoice() {
+	int input;
+	cout << "Pick an option: " << endl;
+	cin >> input;
+
+	switch (input) {
+	case 1:
+		cout << "Starting New Game..... " << endl;
+
+		break;
+	case 2:
+		cout << "Resuming Saved Game....... " << endl;
+		break;
+	case 3:
+		cout << "Deleting Saved game...... " << endl;
+		break;
+	case 4:
+		cout << "Thank You For Playing " << endl;
+		break;
+	case 5:
+		cout << "You've Entered The Game's Settings " << endl;
+		break;
+
+
+	}
+}
+
+
