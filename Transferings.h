9ac9b78e7044256dd7d9c474af4ea788724af585
hayraa19 <62447305+hayraa19@users.noreply.github.com>
2020-03-22 17:49:27 +0000

    Create Transferings.h

---
+++ Transferings.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> liverpoolTeam;
+
+vector<string> chelseaTeam;
+
+void printVector1(const vector<string>& liverpoolTeam);
+
+void printVector2(const vector<string>& chelseaTeam);
+
+
+void fillVector1(vector<string>& liverpoolTeam);
+
+void fillVector2(vector<string>& chelseaTeam);
+
+void choosePlayer(vector<string>& liverpoolTeam, vector<string>& chelseaTeam);
+
+
+
