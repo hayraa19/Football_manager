f12b4eefdea6790cc80b00d73ffcd5b393197045
hayraa19 <62447305+hayraa19@users.noreply.github.com>
2020-03-22 17:50:09 +0000

    Create Transferings.cpp

---
+++ Transferings.cpp
@@ -0,0 +1,69 @@
+#include"Transferings.h"
+#include <string>
+
+using namespace std;
+
+
+
+void fillVector1(vector<string>& liverpoolTeam) {
+
+	liverpoolTeam.push_back("player1");
+	liverpoolTeam.push_back("player2");
+	liverpoolTeam.push_back("player3");
+	liverpoolTeam.push_back("player4");
+}
+
+void fillVector2(vector<string>& chelseaTeam) {
+
+	chelseaTeam.push_back("player1");
+	chelseaTeam.push_back("player2");
+	chelseaTeam.push_back("player3");
+	chelseaTeam.push_back("player4");
+
+
+}
+
+
+void printVector1(const vector<string>& liverpoolTeam) {
+
+	cout << "Liverpool Team" << endl;
+
+	for (unsigned int i = 0; i < liverpoolTeam.size(); i++) {
+		cout << liverpoolTeam[i] << " " << endl;
+
+	}
+
+
+}
+
+void printVector2(const vector<string>& chelseaTeam) {
+
+	cout << "Chelsea Team" << endl;
+
+	for (unsigned int j = 0; j < chelseaTeam.size(); j++) {
+		cout << chelseaTeam[j] << " " << endl;
+	}
+
+}
+
+void choosePlayer(vector<string>& liverpoolTeam, vector<string>& chelseaTeam) {
+	int input;
+	cout << "choose player number to be transfered from liverpool team" << endl;
+	cin >> input;
+
+	if (input == 1) {
+		liverpoolTeam.erase(liverpoolTeam.begin() + 1);
+		chelseaTeam.push_back(liverpoolTeam[0]);
+		
+	}
+	else if (input == 2) {
+		liverpoolTeam.erase(liverpoolTeam.begin() + 2);
+	}
+	else if (input == 3) {
+		liverpoolTeam.erase(liverpoolTeam.begin() + 3);
+	}
+	else if (input == 4) {
+		liverpoolTeam.erase(liverpoolTeam.begin() + 4);
+	}
+}
+
