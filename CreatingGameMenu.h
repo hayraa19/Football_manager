b62505c9916d88ab77756ce532448c6376cc18bd
hayraa19 <62447305+hayraa19@users.noreply.github.com>
2020-03-22 17:36:23 +0000

    Create CreatingGameMenu.h

---
+++ CreatingGameMenu.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#pragma once
+
+// function declarations 
+
+void displayMenu();
+// displayMenu - displays menu options of the game
+
+void enterChoice();
+// enterChoice - tells user to pick an option from the menu and prints appropriate message
+ 
